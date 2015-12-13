733852d8b1abe326899e0d8c314f25b5e1163b36
root <root@ddc-Aspire-4740.(none)>
2015-12-14 00:07:58 +0800

    fix bug

--- netsvr/netctrl.d
+++ netsvr/netctrl.d
@@ -1,6 +1,6 @@
 netctrl.o: netctrl.cpp netctrl.h netdef.h ../sllib/slbase.h \
  ../sllib/slassert.h /usr/include/assert.h /usr/include/features.h \
- /usr/include/i386-linux-gnu/bits/predefs.h \
+ /usr/include/stdc-predef.h /usr/include/i386-linux-gnu/bits/predefs.h \
  /usr/include/i386-linux-gnu/sys/cdefs.h \
  /usr/include/i386-linux-gnu/bits/wordsize.h \
  /usr/include/i386-linux-gnu/gnu/stubs.h \
@@ -16,7 +16,8 @@ netctrl.o: netctrl.cpp netctrl.h netdef.h ../sllib/slbase.h \
  /usr/include/i386-linux-gnu/bits/waitflags.h \
  /usr/include/i386-linux-gnu/bits/waitstatus.h /usr/include/endian.h \
  /usr/include/i386-linux-gnu/bits/endian.h \
- /usr/include/i386-linux-gnu/bits/byteswap.h /usr/include/xlocale.h \
+ /usr/include/i386-linux-gnu/bits/byteswap.h \
+ /usr/include/i386-linux-gnu/bits/byteswap-16.h /usr/include/xlocale.h \
  /usr/include/i386-linux-gnu/sys/types.h /usr/include/time.h \
  /usr/include/i386-linux-gnu/sys/select.h \
  /usr/include/i386-linux-gnu/bits/select.h \
@@ -24,10 +25,10 @@ netctrl.o: netctrl.cpp netctrl.h netdef.h ../sllib/slbase.h \
  /usr/include/i386-linux-gnu/bits/time.h \
  /usr/include/i386-linux-gnu/sys/sysmacros.h \
  /usr/include/i386-linux-gnu/bits/pthreadtypes.h /usr/include/alloca.h \
- /usr/include/string.h /usr/include/errno.h \
- /usr/include/i386-linux-gnu/bits/errno.h /usr/include/linux/errno.h \
- /usr/include/i386-linux-gnu/asm/errno.h /usr/include/asm-generic/errno.h \
- /usr/include/asm-generic/errno-base.h \
+ /usr/include/i386-linux-gnu/bits/stdlib-float.h /usr/include/string.h \
+ /usr/include/errno.h /usr/include/i386-linux-gnu/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/i386-linux-gnu/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
  /usr/include/i386-linux-gnu/bits/timex.h /usr/include/c++/4.6/string \
  /usr/include/c++/4.6/i686-linux-gnu/./bits/c++config.h \
  /usr/include/c++/4.6/i686-linux-gnu/./bits/os_defines.h \
@@ -89,6 +90,7 @@ netctrl.o: netctrl.cpp netctrl.h netdef.h ../sllib/slbase.h \
  /usr/include/i386-linux-gnu/sys/stat.h \
  /usr/include/i386-linux-gnu/bits/stat.h ../sllib/slconfig.h \
  /usr/include/fcntl.h /usr/include/i386-linux-gnu/bits/fcntl.h \
+ /usr/include/i386-linux-gnu/bits/fcntl-linux.h \
  /usr/include/i386-linux-gnu/bits/uio.h \
  /usr/include/i386-linux-gnu/sys/time.h ../sllib/slplatform.h \
  ../sllib/slstring_utils.h ../sllib/sltime.h ../sllib/slbase.h \
@@ -101,6 +103,7 @@ netctrl.o: netctrl.cpp netctrl.h netdef.h ../sllib/slbase.h \
  /usr/include/i386-linux-gnu/sys/socket.h \
  /usr/include/i386-linux-gnu/sys/uio.h \
  /usr/include/i386-linux-gnu/bits/socket.h \
+ /usr/include/i386-linux-gnu/bits/socket_type.h \
  /usr/include/i386-linux-gnu/bits/sockaddr.h \
  /usr/include/i386-linux-gnu/asm/socket.h \
  /usr/include/asm-generic/socket.h \
@@ -117,7 +120,8 @@ netctrl.o: netctrl.cpp netctrl.h netdef.h ../sllib/slbase.h \
  /usr/include/i386-linux-gnu/bits/ioctl-types.h \
  /usr/include/i386-linux-gnu/sys/ttydefaults.h /usr/include/netinet/tcp.h \
  ../sllib/net/../slsingleton.h ../sllib/net/../slsize_string.h \
- /usr/include/i386-linux-gnu/sys/epoll.h ../sllib/net/slsocket.h \
+ /usr/include/i386-linux-gnu/sys/epoll.h \
+ /usr/include/i386-linux-gnu/bits/epoll.h ../sllib/net/slsocket.h \
  ../sllib/slarray.h ../sllib/slshm_queue.h ../sllib/net/slepoll.h \
  ../sllib/ipc/slshm.h ../sllib/ipc/../slconfig.h \
  /usr/include/i386-linux-gnu/sys/ipc.h \
@@ -131,22 +135,13 @@ netctrl.o: netctrl.cpp netctrl.h netdef.h ../sllib/slbase.h \
  /usr/include/i386-linux-gnu/bits/siginfo.h \
  /usr/include/i386-linux-gnu/bits/sigaction.h \
  /usr/include/i386-linux-gnu/bits/sigcontext.h \
- /usr/include/i386-linux-gnu/asm/sigcontext.h /usr/include/linux/types.h \
- /usr/include/i386-linux-gnu/asm/types.h /usr/include/asm-generic/types.h \
- /usr/include/asm-generic/int-ll64.h \
- /usr/include/i386-linux-gnu/asm/bitsperlong.h \
- /usr/include/asm-generic/bitsperlong.h /usr/include/linux/posix_types.h \
- /usr/include/linux/stddef.h \
- /usr/include/i386-linux-gnu/asm/posix_types.h \
- /usr/include/i386-linux-gnu/asm/posix_types_32.h \
  /usr/include/i386-linux-gnu/bits/sigstack.h \
  /usr/include/i386-linux-gnu/sys/ucontext.h \
  /usr/include/i386-linux-gnu/bits/sigthread.h ../sllib/slsize_string.h \
  ../sllib/slsvr_base_frame.h /usr/include/i386-linux-gnu/sys/wait.h \
- /usr/include/i386-linux-gnu/sys/resource.h \
- /usr/include/i386-linux-gnu/bits/resource.h ../sllib/slini_config.h \
- /usr/include/c++/4.6/iostream /usr/include/c++/4.6/ostream \
- /usr/include/c++/4.6/ios /usr/include/c++/4.6/bits/ios_base.h \
+ ../sllib/slini_config.h /usr/include/c++/4.6/iostream \
+ /usr/include/c++/4.6/ostream /usr/include/c++/4.6/ios \
+ /usr/include/c++/4.6/bits/ios_base.h \
  /usr/include/c++/4.6/bits/locale_classes.h \
  /usr/include/c++/4.6/bits/locale_classes.tcc \
  /usr/include/c++/4.6/streambuf /usr/include/c++/4.6/bits/streambuf.tcc \
@@ -164,4 +159,4 @@ netctrl.o: netctrl.cpp netctrl.h netdef.h ../sllib/slbase.h \
  /usr/include/c++/4.6/i686-linux-gnu/./bits/basic_file.h \
  /usr/include/c++/4.6/i686-linux-gnu/./bits/c++io.h \
  /usr/include/c++/4.6/bits/fstream.tcc netclientfactory.h netclient.h \
- netlisten.h netconfig.h
+ netlisten.h netconfig.h flowctrl.h
