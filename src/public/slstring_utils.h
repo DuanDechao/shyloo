89953bbf9f17af0e8cb8eee6a9e83c72749aeac1
ddc <duandechao@shandagame.com>
2016-11-28 21:48:20 +0800

    string untils

--- src/public/slstring_utils.h
+++ src/public/slstring_utils.h
@@ -18,6 +18,7 @@ namespace sl
 		*/
 		static int RemoveNot(std::string& str, const char* pszTrim)
 		{
+
 			size_t iIndex = str.find_first_not_of(pszTrim);
 			int iCount = 0;
 			while(iIndex != std::string::npos)
@@ -110,140 +111,61 @@ namespace sl
 			return str;
 		}
 
-		/**
-        * 严格的十进制整数转换模扳函数.
-        * 1. 支持格式[whitespace][sign]digits[whitespace]\n
-        * 2. @参数iLen为待转化的字符个数，-1表示不定长以\0作为结束标志\n
-        * 3. 返回值为转换后的数字，如果转换失败的话返回0，错误码由piRet接收\n
-        * 4. NULL和空串错误码为-1\n
-        * 5. 含有非+-0123456789字符会错误码为-2\n
-        * 6. 超过数值范围错误码为-3\n
-        */
-        template<typename T>
-        static T StrToInt(const char* pszStr, int iLen = -1, int* piRet = NULL)
-        {
-            const char* pszEnd = (iLen > 0 ? pszStr + iLen -1 : NULL);
-            char* p = (char*) pszStr;
-            if (p == NULL || *p == 0)
-            {
-                if (piRet) 
-                { 
-                    *piRet = -1; 
-                } 
-                return 0;
-            }
+		static string & RepleaceAll(string& str, const string& oldstr, const string& newstr)
+		{
+			do{
+				string::size_type pos(0);
+				if(pos == str.find(oldstr) != string::npos){
+					str.replace(pos, oldstr.length(), newstr);
+				}
+				else{
+					break;
+				}
+			}while(true);
+			return str;
+		}
 
-            const bool bIsUnsigned = (((T)(-1)) > 0);
-            T basedata = 1;
-            T makedata = (T)-1;
-            const T AbsMax = bIsUnsigned ? makedata : (basedata<<(sizeof(T)*8 - 1)^makedata);
-    //        const T AbsMax = bIsUnsigned ? (T)(-1) : (T)((((T)(1))<<(GETTYPEBITSIZE(T)-1))^((T)(-1)));
-    //        const T AbsMax = bIsUnsigned ? (T)(-1) : (T)(((T)(1))<<(sizeof(T)*8-1));
-            const T Overflow = AbsMax / 10;
+		static int32 StringAsInt32(const char* pStr)
+		{
+			SLASSERT(pStr, "is null pointer");
+			return atoi(pStr);
+		}
 
-            // 忽略空白
-            while (*p == ' '&& (pszEnd ? p <= pszEnd : true))
-            {
-                ++p;
-            }
+		static const char* Int32AsString(int32 val)
+		{
+			char str[128] ={0};
+			SafeSprintf(str, 127, "%d", val);
+			return string(str).c_str();
+		}
 
-            int sign = 0; // -1表示以负号开始 1表示以加号开始 0表示以数字开始
-            if (*p == '-' || *p == '+')
-            {
-                if (bIsUnsigned)
-                {
-                    if (piRet) 
-                    { 
-                        *piRet = -2; 
-                    } 
-                    return 0;
-                }
+		static float StringAsFloat(const char* pstr)
+		{
+			SLASSERT(pstr, "is null pointer");
+			return (float)atof(pstr);
+		}
 
-                sign = (*p == '-' ? -1 : 1);
-                ++p;
-            }
+		static const char* FloatAsString(float val)
+		{
+			char str[128] ={0};
+			SafeSprintf(str, 127, "%f", val);
+			return string(str).c_str();
+		}
 
-            // 忽略0
-            while (*p == '0' && (pszEnd ? p <= pszEnd : true))
-            {
-                ++p;
-            }
+		static int64 StringAsInt64(const char* pstr)
+		{
+			SLASSERT(pstr, "is null pointer");
+			return atoll(pstr);
+		}
 
-            T ret = 0;
-            for (; *p != 0 && (pszEnd ? p <= pszEnd : true); ++p)
-            {
-                if (*p >= '0' && *p <= '9')
-                {
-                    if (ret > Overflow) // 整型溢出了
-                    {
-                        if (piRet) 
-                        { 
-                            *piRet = -3; 
-                        } 
-                        return 0;
-                    }
-                    else if (ret == Overflow)
-                    {
-                        if (bIsUnsigned)
-                        {
-                            if (*p > '5')
-                            {
-                                if (piRet) 
-                                { 
-                                    *piRet = -3; 
-                                } 
-                                return 0;
-                            }
-                        }
-                        else
-                        {
-                            if ((sign < 0 && *p > '8') || (sign >= 0 && *p > '7'))
-                            {
-                                if (*p > '5')
-                                {
-                                    if (piRet) 
-                                    { 
-                                        *piRet = -3; 
-                                    } 
-                                    return 0;
-                                }
-                            }
-                        }
-                    }
+		static const char* Int64AsString(int64 val)
+		{
+			char str[128] ={0};
+			SafeSprintf(str, 127, "%lld", val);
+			return string(str).c_str();
+		}
 
-                    ret = ret * 10 + (*p - '0');
-                }
-                else if (*p == ' ')
-                {
-                    // 遇到空格, 则后续都必须是空格才合法
-                    for (; *p != 0 && (pszEnd ? p <= pszEnd : true); ++p)
-                    {
-                        if (*p != ' ')
-                        {
-                            if (piRet) 
-                            { 
-                                *piRet = -2; 
-                            } 
-                            return 0;
-                        }
-                    }
-                    break;
-                }
-                else
-                {
-                    if (piRet) 
-                    { 
-                        *piRet = -2; 
-                    } 
-                    return 0;
-                }
-            }
-            if(piRet)
-            {
-                *piRet = 0;
-            }
-            return (sign < 0 ? 0 - ret : ret);
-        }
+		
+
 	};
 
 }//namespace sl
