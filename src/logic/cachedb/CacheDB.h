ab81223981f702834f17f94e87b3c66dae2005d5
duandechao <duandechao@shandagames.com>
2017-04-21 01:34:13 +0800

    add cachedb module

---
+++ src/logic/cachedb/CacheDB.h
@@ -0,0 +1,50 @@
+#ifndef __SL_CORE_CACHEDB_H__
+#define __SL_CORE_CACHEDB_H__
+#include "ICacheDB.h"
+#include "slsingleton.h"
+#include <unordered_map>
+class CacheDB : public ICacheDB, public sl::SLHolder<CacheDB>{
+	enum{
+		CDB_TYPE_NONE = 0,
+		CDB_TYPE_INT8,
+		CDB_TYPE_INT16,
+		CDB_TYPE_INT32,
+		CDB_TYPE_INT64,
+		CDB_TYPE_STRING,
+
+		CDB_TYPE_CANT_BE_KEY,
+		CDB_TYPE_FLOAT = CDB_TYPE_CANT_BE_KEY,
+		CDB_TYPE_STRUCT,
+		CDB_TYPE_BLOB
+	};
+
+public:
+	struct CacheTable{
+		std::string name;
+		std::unordered_map<std::string, int8> columns;
+		std::string key;
+		bool del;
+	};
+public:
+	virtual bool initialize(sl::api::IKernel * pKernel);
+	virtual bool launched(sl::api::IKernel * pKernel);
+	virtual bool destory(sl::api::IKernel * pKernel);
+
+	virtual bool read(const char* table, const CacheDBColumnFuncType& cf, const CacheDBReadFuncType& f, int32 count, ...);
+	virtual bool readByIndex(const char* table, const CacheDBColumnFuncType& cf, const CacheDBReadFuncType& f, const int64 index);
+	virtual bool readByIndex(const char* table, const CacheDBColumnFuncType& cf, const CacheDBReadFuncType& f, const char* index);
+
+	virtual bool write(const char* table, const CacheDBWriteFuncType& f, int32 count, ...);
+	virtual bool writeByIndex(const char* table, const CacheDBWriteFuncType& f, const int64 index);
+	virtual bool writeByIndex(const char* table, const CacheDBWriteFuncType& f, const char* index);
+
+	virtual bool del(const char* table, int32 count, ...);
+	virtual bool delByIndex(const char* table, const int64 index);
+	virtual bool delByIndex(const char* table, const char* index);
+
+private:
+	sl::api::IKernel*		_kernel;
+
+	std::unordered_map<std::string, CacheTable> _tables;
+};
+#endif
