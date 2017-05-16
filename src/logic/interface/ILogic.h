bc75dcdae142289167adad7073d59f54043bb43e
ddc <duandechao@shandagame.com>
2017-05-16 22:01:53 +0800

    fix logic bug

--- src/logic/interface/ILogic.h
+++ src/logic/interface/ILogic.h
@@ -1,8 +1,21 @@
 #ifndef _SL_INTERFACE_LOGIC_H__
 #define _SL_INTERFACE_LOGIC_H__
 #include "slimodule.h"
-class ILogic : public sl::api::IModule
-{
+#include "slbinary_stream.h"
+class IObject;
+class IProtocolHandler{
+public:
+	virtual ~IProtocolHandler() {}
+	virtual bool dealProtocol(sl::api::IKernel* pKernel, IObject* object, const void* context, const int32 size) = 0;
+};
+
+class BProtocolHandler{
+public:
+	typedef std::function<bool(sl::api::IKernel* pKernel, IObject* object, const sl::OBStream& args)> HandleFunctionType;
+	BProtocolHandler
+};
+
+class ILogic : public sl::api::IModule{
 public:
 	virtual ~ILogic() {}
 };
