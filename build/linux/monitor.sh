a0edc53a337a61f40651cc6a3464d86e24bd2219
ddc <ddc_cug@163.com>
2017-07-08 21:29:34 +0800

    debug on linux

---
+++ build/linux/monitor.sh
@@ -0,0 +1,1 @@
+./shyloo --name=monitor --node_id=1 --server_ip=127.0.0.1 --server_port=12580 --func=0 &
---
+++ build/linux/robot.sh
@@ -0,0 +1,1 @@
+./shyloo --name=robot --node_id=1 --server_ip=127.0.0.1 --server_port=7100 --account=ddct --role=fancty &
